fc856b773013bb45def76882d1739228eec53173
fcosmeo <frankcosmesystem@gmail.com>
2017-10-10 10:39:16 -0500

    valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  aaa: number;
+
+  constructor(
+  ) {
+    this.aaa = 5;
+  }
+
+  avanza() {
+    this.aaa = this.aaa + 5;
+    //for (var _i = 0; _i < 20; _i++) {
+      //this.aaa = this.aaa + 1;
+    //}
+  }
+
 }
